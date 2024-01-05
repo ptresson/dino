c73c2ae52cafe4afb40237299f604cbda1f53905
ptresson <paul.tresson@ird.fr>
2024-01-05 14:43:19 +0100

    data augmentation compatible with torchgeo (cf. dinov2 repo)

--- main_dino.py
+++ main_dino.py
@@ -30,9 +30,16 @@ import torch.nn.functional as F
 from torchvision import datasets, transforms
 from torchvision import models as torchvision_models
 
+from torchgeo.datasets import RasterDataset, stack_samples, unbind_samples, BoundingBox
+from torchgeo.samplers import RandomGeoSampler, GridGeoSampler
+from torchgeo.transforms import AugmentationSequential
+import kornia.augmentation as K
+from kornia.enhance.normalize import Normalize
+
 import utils
 import vision_transformer as vits
 from vision_transformer import DINOHead
+
 
 torchvision_archs = sorted(name for name in torchvision_models.__dict__
     if name.islower() and not name.startswith("__")
@@ -416,53 +423,144 @@ class DINOLoss(nn.Module):
         self.center = self.center * self.center_momentum + batch_center * (1 - self.center_momentum)
 
 
-class DataAugmentationDINO(object):
-    def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
-        flip_and_color_jitter = transforms.Compose([
-            transforms.RandomHorizontalFlip(p=0.5),
-            transforms.RandomApply(
-                [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
-                p=0.8
-            ),
-            transforms.RandomGrayscale(p=0.2),
-        ])
-        normalize = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ])
+# class DataAugmentationDINO(object):
+#     def __init__(self, global_crops_scale, local_crops_scale, local_crops_number):
+#         flip_and_color_jitter = transforms.Compose([
+#             transforms.RandomHorizontalFlip(p=0.5),
+#             transforms.RandomApply(
+#                 [transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
+#                 p=0.8
+#             ),
+#             transforms.RandomGrayscale(p=0.2),
+#         ])
+#         normalize = transforms.Compose([
+#             transforms.ToTensor(),
+#             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+#         ])
 
-        # first global crop
-        self.global_transfo1 = transforms.Compose([
-            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
-            flip_and_color_jitter,
-            utils.GaussianBlur(1.0),
-            normalize,
-        ])
-        # second global crop
-        self.global_transfo2 = transforms.Compose([
-            transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
-            flip_and_color_jitter,
-            utils.GaussianBlur(0.1),
-            utils.Solarization(0.2),
-            normalize,
-        ])
-        # transformation for the local small crops
+#         # first global crop
+#         self.global_transfo1 = transforms.Compose([
+#             transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+#             flip_and_color_jitter,
+#             utils.GaussianBlur(1.0),
+#             normalize,
+#         ])
+#         # second global crop
+#         self.global_transfo2 = transforms.Compose([
+#             transforms.RandomResizedCrop(224, scale=global_crops_scale, interpolation=Image.BICUBIC),
+#             flip_and_color_jitter,
+#             utils.GaussianBlur(0.1),
+#             utils.Solarization(0.2),
+#             normalize,
+#         ])
+#         # transformation for the local small crops
+#         self.local_crops_number = local_crops_number
+#         self.local_transfo = transforms.Compose([
+#             transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
+#             flip_and_color_jitter,
+#             utils.GaussianBlur(p=0.5),
+#             normalize,
+#         ])
+
+#     def __call__(self, image):
+#         crops = []
+#         crops.append(self.global_transfo1(image))
+#         crops.append(self.global_transfo2(image))
+#         for _ in range(self.local_crops_number):
+#             crops.append(self.local_transfo(image))
+#         return crops
+
+class GaussianBlur(transforms.RandomApply):
+    def __init__(self, *, p: float = 0.5, radius_min: float = 0.1, radius_max: float = 2.0):
+        # NOTE: torchvision is applying 1 - probability to return the original image
+        keep_p = 1 - p
+        transform = K.RandomGaussianBlur(kernel_size=9, sigma=(radius_min, radius_max), p=p)     
+        super().__init__(transforms=[transform], p=keep_p)
+
+
+class DataAugmentationDINOTorchgeo(object):
+    def __init__(
+        self,
+        global_crops_scale,
+        local_crops_scale,
+        local_crops_number,
+        global_crops_size=224,
+        local_crops_size=96,
+        #dataset_mean = 0,
+        #dataset_std = 1
+    ):
+        self.global_crops_scale = global_crops_scale
+        self.local_crops_scale = local_crops_scale
         self.local_crops_number = local_crops_number
-        self.local_transfo = transforms.Compose([
-            transforms.RandomResizedCrop(96, scale=local_crops_scale, interpolation=Image.BICUBIC),
-            flip_and_color_jitter,
-            utils.GaussianBlur(p=0.5),
-            normalize,
-        ])
+        self.global_crops_size = global_crops_size
+        self.local_crops_size = local_crops_size
+
+        
+        # random resized crop and flip
+        self.geometric_augmentation_global = AugmentationSequential(
+                #K.RandomResizedCrop(size=global_crops_size, scale=global_crops_scale),
+                transforms.RandomResizedCrop(
+                    global_crops_size, scale=global_crops_scale, interpolation=transforms.InterpolationMode.BICUBIC
+                ),
+                K.RandomHorizontalFlip(p=0.5),
+                data_keys=["image"]
+        )
+
+        self.geometric_augmentation_local = AugmentationSequential(
+                #K.RandomResizedCrop(size=local_crops_size, scale=local_crops_scale),
+                transforms.RandomResizedCrop(
+                    global_crops_size, scale=global_crops_scale, interpolation=transforms.InterpolationMode.BICUBIC
+                ),
+                K.RandomHorizontalFlip(p=0.5),
+                data_keys=["image"]
+        )
+
+        IMAGESET_DEFAULT_MEAN = [101.46047364732716]       #TODO : needs to be determined dynamically           #dataset_mean 
+        IMAGESET_DEFAULT_STD = [18.518984529105996]       #TODO : needs to be determined dynamically            #dataset_std    
+
+        def make_normalize_transform(
+            mean = IMAGESET_DEFAULT_MEAN,
+            std = IMAGESET_DEFAULT_STD,
+        ):
+            return Normalize(mean=mean, std=std)            
+    
+        # normalization
+        self.normalize = AugmentationSequential(
+                make_normalize_transform(),
+                data_keys=["image"],
+        )
+      
+        self.global_transfo1 = AugmentationSequential(GaussianBlur(p=1.0), data_keys=["image"])  #make_normalize_transform(),
+        self.global_transfo2 = AugmentationSequential(GaussianBlur(p=0.1), 
+                                                       transforms.RandomSolarize(threshold=0, p=0.2), 
+                                                        data_keys=["image"])   #make_normalize_transform(),
+        #self.global_transfo2 = AugmentationSequential(GaussianBlur(p=0.1), K.RandomBrightness(p=0.5), K.RandomInvert(p=0.5), data_keys=["image"])
+        self.local_transfo = AugmentationSequential(GaussianBlur(p=0.5), data_keys=["image"])       #make_normalize_transform(),
+        
 
     def __call__(self, image):
-        crops = []
-        crops.append(self.global_transfo1(image))
-        crops.append(self.global_transfo2(image))
-        for _ in range(self.local_crops_number):
-            crops.append(self.local_transfo(image))
-        return crops
+        output = {}
 
+        image = self.normalize(image)
+        # global crops:
+        im1_base = self.geometric_augmentation_global(image)
+        global_crop_1 = self.global_transfo1(im1_base)
+
+        im2_base = self.geometric_augmentation_global(image)
+        print('im2 base : ', im2_base)
+        global_crop_2 = self.global_transfo2(im2_base)
+        output["global_crops"] = [global_crop_1, global_crop_2]
+        print('output global crops im2 : ', output['global_crops'][1])
+        # global crops for teacher:
+        output["global_crops_teacher"] = [global_crop_1, global_crop_2]
+        # local crops:
+        local_crops = [
+            self.local_transfo(self.geometric_augmentation_local(image)) for _ in range(self.local_crops_number)
+        ]
+        output["local_crops"] = local_crops
+        output["offsets"] = ()
+
+        return output
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('DINO', parents=[get_args_parser()])
