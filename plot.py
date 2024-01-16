daee39964005816592108b488315272bbeb2c5c5
ptresson <paul.tresson@ird.fr>
2024-01-16 11:03:04 +0100

    handle coherent axis limits

--- plot.py
+++ plot.py
@@ -24,7 +24,7 @@ def plot_umap(checkpoint="dino_vitb16", xlim=[-7, 32], ylim=[-5,25]):
             data=merged_gdf, 
             palette='Spectral', 
             ax=ax1, 
-            s=10
+            s=20
             )
     scatter_src.legend(loc='lower left',ncol=1, title="src", bbox_to_anchor=(1, 0))
     # ax1.set_title('Points Colored by src')
@@ -36,24 +36,105 @@ def plot_umap(checkpoint="dino_vitb16", xlim=[-7, 32], ylim=[-5,25]):
             data=merged_gdf, 
             palette='tab10', 
             ax=ax2, 
-            s=10
+            s=20
             )
     scatter_ID.legend(loc='lower left', title="Class", bbox_to_anchor=(1, 0))
     # ax2.set_title('Points Colored by kmeans')
 
-    if xlim and ylim:
-        scatter_src.xlim(xlim)
-        scatter_src.ylim(ylim)
-        scatter_ID.xlim(xlim)
-        scatter_ID.ylim(ylim)
+    scatter_src.set_xlim(xlim)
+    scatter_src.set_ylim(ylim)
+    scatter_ID.set_xlim(xlim)
+    scatter_ID.set_ylim(ylim)
     
     plt.tight_layout()
 
     # Save the plot to a file
-    fig.savefig(f'./out/plots/{checkpoint}.png')
+    fig.savefig(f'./out/plots/umap/{checkpoint}.png')
+    plt.close()
+
+
+def plot_pca(checkpoint="dino_vitb16", xlim=[-7, 32], ylim=[-5,25]):
+    # Load your shapefiles into GeoDataFrames
+    gdf1 = gpd.read_file(f'./out/A/{checkpoint}.shp')
+    gdf2 = gpd.read_file(f'./out/B/{checkpoint}.shp')
+
+    # Concatenate GeoDataFrames vertically
+    merged_gdf = pd.concat([gdf1, gdf2], ignore_index=True)
+    merged_gdf['src'] = merged_gdf['src'].apply(lambda x: os.path.splitext(os.path.basename(x))[0])  # Keeps only the filename without extension
+
+
+    # Create a shared figure and two subplots
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4))
+
+    scatter_src = sns.scatterplot(
+            x='x_pca', 
+            y='y_pca', 
+            hue='src', 
+            data=merged_gdf, 
+            palette='Spectral', 
+            ax=ax1, 
+            s=20
+            )
+    scatter_src.legend(loc='lower left',ncol=1, title="src", bbox_to_anchor=(1, 0))
+    # ax1.set_title('Points Colored by src')
+    
+    scatter_ID = sns.scatterplot(
+            x='x_pca', 
+            y='y_pca', 
+            hue='C_ID', 
+            data=merged_gdf, 
+            palette='tab10', 
+            ax=ax2, 
+            s=20
+            )
+    scatter_ID.legend(loc='lower left', title="Class", bbox_to_anchor=(1, 0))
+    # ax2.set_title('Points Colored by kmeans')
+
+    scatter_src.set_xlim(xlim)
+    scatter_src.set_ylim(ylim)
+    scatter_ID.set_xlim(xlim)
+    scatter_ID.set_ylim(ylim)
+    
+    plt.tight_layout()
+
+    # Save the plot to a file
+    fig.savefig(f'./out/plots/pca/{checkpoint}.png')
+    plt.close()
+
+
+def get_lim(arch="dino_vitb16", method='umap', margin=0.10):
+    
+    total_df = None
+    gdf1 = gpd.read_file(f'./out/A/{arch}0000.shp')
+    gdf2 = gpd.read_file(f'./out/B/{arch}0000.shp')
+    total_df = pd.concat([gdf1, gdf2], ignore_index=True)
+
+    for step in ['0020','0040','0060','0080','']:
+        gdf1 = gpd.read_file(f'./out/A/{arch}{step}.shp')
+        gdf2 = gpd.read_file(f'./out/B/{arch}{step}.shp')
+        merged_df = pd.concat([gdf1, gdf2], ignore_index=True)
+        total_df = pd.concat([total_df, merged_df], ignore_index=True)
+        
+        xmargin = abs(total_df[f'x_{method}'].max() - total_df[f'x_{method}'].min()) * margin
+        ymargin = abs(total_df[f'y_{method}'].max() - total_df[f'y_{method}'].min()) * margin
+        x_min = total_df[f'x_{method}'].min() - xmargin
+        x_max = total_df[f'x_{method}'].max() + xmargin
+        y_min = total_df[f'y_{method}'].min() - ymargin
+        y_max = total_df[f'y_{method}'].max() + ymargin
+
+    return [x_min, x_max],[y_min, y_max]
+    
 
 
 if __name__ == "__main__":
-    for arch in ['dino_vitb16']:
+
+    # for arch in ['dino_vitb16', 'dino_resnet50', 'efficientnetb0', 'efficientnetb3']:
+    # for arch in ['dino_vitb16', 'dino_resnet50', 'efficientnetb0', 'efficientnetb3']:
+    for arch in ['dino_vitb16', 'dino_resnet50']:
+
+        xlim_umap, ylim_umap = get_lim(arch)
+        xlim_pca, ylim_pca = get_lim(arch, method='pca')
+
         for step in ['0000','0020','0040','0060','0080','']:
-            plot_umap(f'{arch}{step}')
+            plot_umap(f'{arch}{step}', xlim_umap, ylim_umap)
+            plot_pca(f'{arch}{step}', xlim_pca, ylim_pca)
